a726abb31e31dee9a79d97ceee3989b1646025c1
Uli Kusterer <ulivc@zathras.de>
2013-02-20 21:58:26 +0100

    Made fake Handle stuff build.

--- ReClassicfication/RCLAppDelegate.m
+++ ReClassicfication/RCLAppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "RCLAppDelegate.h"
+#import "MacMemory.h"
+#import "FakeHandles.h"
+
 
 @implementation RCLAppDelegate
 
@@ -17,7 +20,29 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-	// Insert code here to initialize your application
+	Handle		theHand;
+	long		theSize;
+	char		num;
+	
+	InitFakeHandles( masterPointers );
+	
+	theHand = NewFakeHandle( 2 );
+	
+	(**(short**)theHand) = -1024;
+	
+	printf( "The number is: %d\n", (**(short**)theHand) );
+	
+	theSize = GetFakeHandleSize( theHand );
+	printf( "Current Size: %ld\n", theSize );
+	
+	num = ((**(short**)theHand) & 0xFF00) >> 8;
+	
+	theSize -= 1;
+	SetFakeHandleSize( theHand, theSize );
+	theSize = GetFakeHandleSize( theHand );
+	printf( "New Size: %ld\n", theSize );
+	
+	printf( "The number is: %d (%d)\n", (**(char**)theHand), num );
 }
 
 @end
